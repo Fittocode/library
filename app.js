92ada024f4e9a9357e776ee466c217abb9968bb0
Matt Harrison <MattHarrison92@gmail.com>
2022-01-28 14:13:38 -0600

    fix closed mongoose connection

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const index = require('./routes/index');
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
+
+// middleware
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/Book.model');
+const mongoose = require('mongoose');
+
+const DB_NAME = 'library-project';
+
+mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 router.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+router.get('/books', async (req, res) => {
+  const books = await Book.find();
+  try {
+    res.render('books-list', { books: books });
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 module.exports = router;
